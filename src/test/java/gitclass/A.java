5e3f374839e4dbeba72a83de94b4ecdef549bb5f
GITHUB <pkindirakumar21@gmail.com>
2022-11-14 12:48:32 +0530

    add two method

--- src/test/java/gitclass/A.java
+++ src/test/java/gitclass/A.java
@@ -11,6 +11,16 @@ public class A {
 		System.out.println("3");
 
 	}
+	public void method5() {
+		System.out.println("5");
+
+	}
+	public void method6() {
+		System.out.println("6");
+
+	}
+
+
 
 	}
 
